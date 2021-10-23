eda83254383a8b7db1376cbcccfd5d665fe73105
kumackey <kumak1t09e0@gmail.com>
2021-10-23 22:19:21 +0900

    refactor: suiteの書き方に変更

--- int_closed_range_test.go
+++ int_closed_range_test.go
@@ -1,36 +1,41 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-func Test_整数閉区間は下端点と上端点を持つ(t *testing.T) {
-	icRange := IntClosedRange{3, 7}
-	assert.Equal(t, 3, icRange.Lower())
-	assert.Equal(t, 7, icRange.Upper())
+type intClosedRangeSuite struct {
+	suite.Suite
+	icRange IntClosedRange
 }
 
-func Test_整数閉区間の文字列表記を返せる(t *testing.T) {
-	icRange := IntClosedRange{3, 7}
-	assert.Equal(t, "[3,7]", icRange.Notation())
-
-	icRange = IntClosedRange{2, 8}
-	assert.Equal(t, "[2,8]", icRange.Notation())
+func (suite *intClosedRangeSuite) SetupSuite() {
+	suite.icRange = IntClosedRange{3, 7}
 }
 
-func Test_整数閉区間は指定した整数を含むか判定できる(t *testing.T) {
-	icRange := IntClosedRange{3, 7}
-	assert.True(t, icRange.Includes(5))
-	assert.False(t, icRange.Includes(1))
+func (suite *intClosedRangeSuite) Test_整数閉区間は下端点と上端点を持つ() {
+	suite.Equal(3, suite.icRange.Lower())
+	suite.Equal(7, suite.icRange.Upper())
 }
 
-func Test_整数閉区間は下端点3を含む(t *testing.T) {
-	icRange := IntClosedRange{3, 7}
-	assert.True(t, icRange.Includes(3))
+func (suite *intClosedRangeSuite) Test_整数閉区間の文字列表記を返せる() {
+	suite.Equal("[3,7]", suite.icRange.Notation())
 }
 
-func Test_整数閉区間は上端点7を含む(t *testing.T) {
-	icRange := IntClosedRange{3, 7}
-	assert.True(t, icRange.Includes(7))
+func (suite *intClosedRangeSuite) Test_整数閉区間は指定した整数を含むか判定できる() {
+	suite.True(suite.icRange.Includes(5))
+	suite.False(suite.icRange.Includes(1))
+}
+
+func (suite *intClosedRangeSuite) Test_整数閉区間は下端点3を含む() {
+	suite.True(suite.icRange.Includes(3))
+}
+
+func (suite *intClosedRangeSuite) Test_整数閉区間は上端点7を含む() {
+	suite.True(suite.icRange.Includes(7))
+}
+
+func TestIntClosedRangeSuite(t *testing.T) {
+	suite.Run(t, new(intClosedRangeSuite))
 }
