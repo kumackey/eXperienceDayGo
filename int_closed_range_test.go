b6d2ed4763d8e152b82106a2651e1b04e9a8b2c8
kumackey <kumak1t09e0@gmail.com>
2021-10-23 18:45:33 +0900

    refactor: 日本語テスト名にしてしまう

--- int_closed_range_test.go
+++ int_closed_range_test.go
@@ -5,13 +5,13 @@ import (
 	"testing"
 )
 
-func TestIntClosedRange_LowerAndUpper(t *testing.T) {
+func Test_整数閉区間は下端点と上端点を持つ(t *testing.T) {
 	icRange := IntClosedRange{3, 7}
 	assert.Equal(t, 3, icRange.Lower())
 	assert.Equal(t, 7, icRange.Upper())
 }
 
-func TestIntClosedRange_Notaion(t *testing.T) {
+func Test_整数閉区間の文字列表記を返せる(t *testing.T) {
 	icRange := IntClosedRange{3, 7}
 	assert.Equal(t, "[3,7]", icRange.Notation())
 
@@ -19,9 +19,18 @@ func TestIntClosedRange_Notaion(t *testing.T) {
 	assert.Equal(t, "[2,8]", icRange.Notation())
 }
 
-func TestIntClosedRange_Includes(t *testing.T) {
+func Test_整数閉区間は指定した整数を含むか判定できる(t *testing.T) {
 	icRange := IntClosedRange{3, 7}
 	assert.True(t, icRange.Includes(5))
+	assert.False(t, icRange.Includes(1))
+}
+
+func Test_整数閉区間は下端点3を含む(t *testing.T) {
+	icRange := IntClosedRange{3, 7}
 	assert.True(t, icRange.Includes(3))
-	assert.False(t, icRange.Includes(2))
+}
+
+func Test_整数閉区間は上端点7を含む(t *testing.T) {
+	icRange := IntClosedRange{3, 7}
+	assert.True(t, icRange.Includes(7))
 }
