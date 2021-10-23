0f2de21b4363c78b52fc9ae7bfb4f370d1c8b004
kumackey <kumak1t09e0@gmail.com>
2021-10-23 18:06:35 +0900

    fix: 最小限のファイルに修正

--- main.go
+++ main.go
@@ -1,43 +1,7 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"sync"
-)
-
-var wg sync.WaitGroup
-
-func generator(ctx context.Context, num int) <-chan int {
-	out := make(chan int)
-	go func() {
-		defer wg.Done()
-
-	LOOP:
-		for {
-			select {
-			case <-ctx.Done():
-				break LOOP
-			case out <- num: // キャンセルされてなければnumを送信
-			}
-		}
-
-		close(out)
-		fmt.Println("generator closed")
-	}()
-	return out
-}
+import "fmt"
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	gen := generator(ctx, 1)
-
-	wg.Add(1)
-
-	for i := 0; i < 5; i++ {
-		fmt.Println(<-gen)
-	}
-	cancel()
-
-	wg.Wait()
+	fmt.Println("hello world")
 }
